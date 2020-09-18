97d1896cbdd472b8d458bb84681e593bb08d3338
Rupinder Ghotra <rghotra@blackberry.com>
2020-09-18 11:49:07 -0400

    Insertion Sort
    Change-Id: Id61f7af769ab2daba3946f8fbeab0801e2ff0f8b

---
+++ Sorting/InsertionSort/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/*
+Insertion Sort:
+
+What Type of Sorting:
+
+Time Complexity: O(n^2)
+
+
+*/
+
+void insertion_sort(int arr[]) {
+
+}
+
+int main() {
+    //Learn how to take input from user.
+
+    int array1[] = {21, 31, 2, 7, 11, 45, 19, 20};
+    std::cout << "Hello Insertion Sort" << std::endl;
+    insertion_sort(array1);
+    return 0;
+}
