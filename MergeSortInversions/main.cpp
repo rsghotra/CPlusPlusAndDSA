a0826ada8419a3cecd7e5c355966ae285f531345
Rupinder Ghotra <rghotra@blackberry.com>
2020-09-24 10:56:06 -0400

    Merge Sort Inversion in O(nlogn) time
    Change-Id: Ic91111088c52075a3f7ad682273ce030abf9204d

--- MergeSortInversions/main.cpp
+++ MergeSortInversions/main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 // 4. Because if it is coming out RIGHT ARRAY THEN J > I BUT A[I] > A[J]
 // 5. WHICH MEANS RECURSIVE FUNCTION AND ANY HELPER FUNCTION MUST BE RETURNING NO OF INVERSIONS
 // 6. Every sub problem must return an int and then add the two inversions
+// 7. Ref 1: Coreman 3rd Editin
+// 8. Ref 2: https://www.geeksforgeeks.org/counting-inversions/ --> Solution is similar but not exactly same.
 // */
 
 int merge(int a1[], int low, int mid, int high) {
@@ -29,6 +31,7 @@ int merge(int a1[], int low, int mid, int high) {
         right[j] = a1[j+mid+1];
     }
     i = 0; j = 0;
+    //PIT FALL
     k = low;
     //Now the Merge;
     while(i < n1 && j < n2) {
@@ -37,8 +40,11 @@ int merge(int a1[], int low, int mid, int high) {
             i++;
         } else {
             a1[k] = right[j];
+            cout << "=========" << " i= " << i << " j= " << j << " =================" << endl;
+            cout << "Left[i]: " << left[i] << endl;
+            cout << "Right[i]: " << right[j] << endl;
             j++;
-            inversions += mid - i;
+            inversions += mid - i + 1;
         }
         k++;
     }
@@ -52,7 +58,6 @@ int merge(int a1[], int low, int mid, int high) {
         a1[k] = right[j];
         j++;
         k++;
-        inversions++;
     }
     return inversions;
 }
@@ -70,7 +75,7 @@ int mergesort(int a1[], int leftIndex, int rightIndex) {
 
 int main() {
     int SIZE = 5;
-    int array[] = { 1, 20, 6, 4, 5};
+    int array[] = { 1, 2, 3, 4, 5 };
     
     for(int i = 0; i < SIZE; i++) {
         cout << array[i] << " ";
@@ -85,94 +90,3 @@ int main() {
     cout << "Number of Inversions: " << inversions << endl;
     return 0;
 }
-
-// C++ program to Count 
-// Inversions in an array 
-// using Merge Sort 
-
-// int _mergeSort(int arr[], int temp[], int left, int right); 
-// int merge(int arr[], int temp[], int left, int mid, int right); 
-
-// /* This function sorts the input array and returns the 
-// number of inversions in the array */
-// int mergeSort(int arr[], int array_size) 
-// { 
-// 	int temp[array_size]; 
-// 	return _mergeSort(arr, temp, 0, array_size - 1); 
-// } 
-
-// /* An auxiliary recursive function that sorts the input array and 
-// returns the number of inversions in the array. */
-// int _mergeSort(int arr[], int temp[], int left, int right) 
-// { 
-// 	int mid, inv_count = 0; 
-// 	if (right > left) { 
-// 		/* Divide the array into two parts and 
-// 		call _mergeSortAndCountInv() 
-// 		for each of the parts */
-// 		mid = (right + left) / 2; 
-
-// 		/* Inversion count will be sum of 
-// 		inversions in left-part, right-part 
-// 		and number of inversions in merging */
-// 		inv_count += _mergeSort(arr, temp, left, mid); 
-// 		inv_count += _mergeSort(arr, temp, mid + 1, right); 
-
-// 		/*Merge the two parts*/
-// 		inv_count += merge(arr, temp, left, mid + 1, right); 
-// 	} 
-// 	return inv_count; 
-// } 
-
-// /* This funt merges two sorted arrays 
-// and returns inversion count in the arrays.*/
-// int merge(int arr[], int temp[], int left, 
-// 		int mid, int right) 
-// { 
-// 	int i, j, k; 
-// 	int inv_count = 0; 
-
-// 	i = left; /* i is index for left subarray*/
-// 	j = mid; /* j is index for right subarray*/
-// 	k = left; /* k is index for resultant merged subarray*/
-// 	while ((i <= mid - 1) && (j <= right)) { 
-// 		if (arr[i] <= arr[j]) { 
-// 			temp[k++] = arr[i++]; 
-// 		} 
-// 		else { 
-// 			temp[k++] = arr[j++]; 
-
-// 			/* this is tricky -- see above 
-// 			explanation/diagram for merge()*/
-// 			inv_count = inv_count + (mid - i); 
-// 		} 
-// 	} 
-
-// 	/* Copy the remaining elements of left subarray 
-// (if there are any) to temp*/
-// 	while (i <= mid - 1) 
-// 		temp[k++] = arr[i++]; 
-
-// 	/* Copy the remaining elements of right subarray 
-// (if there are any) to temp*/
-// 	while (j <= right) 
-// 		temp[k++] = arr[j++]; 
-
-// 	/*Copy back the merged elements to original array*/
-// 	for (i = left; i <= right; i++) 
-// 		arr[i] = temp[i]; 
-
-// 	return inv_count; 
-// } 
-
-// // Driver code 
-// int main() 
-// { 
-// 	int arr[] = { 1,2,3,4,5,6 }; 
-// 	int n = sizeof(arr) / sizeof(arr[0]); 
-// 	int ans = mergeSort(arr, n); 
-// 	cout << " Number of inversions are " << ans; 
-// 	return 0; 
-// } 
-
-// // This is code is contributed by rathbhupendra 
